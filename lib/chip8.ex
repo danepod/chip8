1bb64e59b487bb3f7c5c73264c707663e03f9c5b
Dane Podzkiewitz <dane.podzkiewitz@gmail.com>
2020-11-15 01:50:13 +0100

    Use nicer decode function for disassembling ROMs

--- lib/chip8.ex
+++ lib/chip8.ex
@@ -24,7 +24,7 @@ defmodule Chip8 do
     disasm =
       Enum.with_index(
         for <<byte1::8, byte2::8 <- binary>> do
-          tick(byte1, byte2)
+          VM.decode(<<byte1, byte2>>)
         end
       )
       |> Enum.map(fn {disasm, pc} ->
@@ -41,21 +41,5 @@ defmodule Chip8 do
     {:ok, binary} = File.read(filename <> ".ch8")
 
     binary
-  end
-
-  defp tick(byte1, byte2) do
-    hi1 = Bitwise.>>>(byte1, 4)
-    lo1 = Bitwise.&&&(byte1, 0x0F)
-
-    # hi1 |> IO.inspect()
-    # lo1 |> IO.inspect()
-
-    hi2 = Bitwise.>>>(byte2, 4)
-    lo2 = Bitwise.&&&(byte2, 0x0F)
-
-    # hi2 |> IO.inspect()
-    # lo2 |> IO.inspect()
-
-    Disassembler.disassemble(<<hi1, lo1, hi2, lo2>>)
   end
 end
